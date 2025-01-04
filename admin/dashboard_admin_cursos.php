410d3143fd672bbae25710e72ad55a5eb33a1f3c
Luis Ake <luis.ake142002@gmail.com>
2025-01-03 18:26:01 -0600

    añadir_cursosdashboard

--- admin/dashboard_admin_cursos.php
+++ admin/dashboard_admin_cursos.php
@@ -31,7 +31,35 @@ if (!isset($_SESSION["autentificado"]) || $_SESSION["autentificado"] != "SI") {
         <br>
         <a href="agregar_curso.php" class="btn_menu1">Agregar Cursos</a>
         <br>
-        
+        <div class="py-3 contenedor-cursos">
+            <?php
+            require "../conexion.php";
+
+            $todos_cursos = "SELECT * FROM clase 
+                                INNER JOIN registro_maestros ON clase.id_maestros = registro_maestros.id_maestros 
+                                INNER JOIN grupo ON clase.id_grupo = grupo.id_grupo;";
+            $resultado = mysqli_query($conectar, $todos_cursos);
+            while($fila_curso = mysqli_fetch_assoc($resultado)){
+            ?>
+            <!-- tarjeta de los cursos -->
+            <div class="card  tarjeta-curso">
+                <div class="card-header">
+                    <h3 class="card-title"><?php echo $fila_curso["nombre_clase"]?></h3>
+                </div>
+                <div class="card-body">
+                    <p><b>Tipo:</b> <?php echo $fila_curso["tipo"]?></p>
+                    <p><b>Docente:</b> <?php echo $fila_curso["nombre"]?><?php echo $fila_curso["apellidos"]?></p>
+                    <p><b>Cupos disponibles:</b> <?php echo $fila_curso["cupos"]?></p>
+                    <p><b>Grupo:</b> <?php echo $fila_curso["nombre_grupo"]?></p>
+                    <p><b>Descripcion:</b> <?php echo $fila_curso["descripcion"]?></p>
+                </div>
+                <div class="acciones-curso">
+                    <a href="editar_curso.php?id_clase=<?php echo $fila_curso['id_clase'];?>" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i>&nbspEditar</a>
+                    <a href="eliminar_curso.php?id_clase=<?php echo $fila_curso['id_clase'];?>" class="btn btn-danger"><i class="fa-solid fa-trash-can" aria-hidden="true"></i>&nbspEliminar</a>
+                </div>
+            </div>
+            <?php }?>
+        </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
--- admin/guardar_curso.php
+++ admin/guardar_curso.php
@@ -4,19 +4,31 @@ require "../conexion.php";
 
 $tipo_curso = $_POST['tipo_curso'];
 $maestro = $_POST['maestro'];
+$grupo = $_POST['grupo'];
 $nombre_curso = $_POST['nombre_curso'];
 $descripcion = $_POST['descripcion'];
+$cupos = $_POST['cupos'];
 $fecha_inicio = $_POST['fecha_inicio'];
 $fecha_fin = $_POST['fecha_fin'];
 
 
 // echo $tipo_curso. "<br>";
 // echo $maestro. "<br>";
+// echo $grupo. "<br>";
 // echo $nombre_curso. "<br>";
 // echo $descripcion. "<br>";
 // echo $fecha_inicio. "<br>";
 // echo $fecha_fin. "<br>";
 
+$insertar = "INSERT INTO clase (id_grupo, tipo, nombre_clase, descripcion, fecha_inicio, fecha_fin, cupos, id_maestros)
+VALUES ('$grupo', '$tipo_curso', '$nombre_curso', '$descripcion', '$fecha_inicio', '$fecha_fin', '$cupos', '$maestro')";
+$resultado = mysqli_query($conectar, $insertar);
+
+if($resultado){
+    echo "<script>alert('Se ha guardado el curso correctamente'); window.location='dashboard_admin_cursos.php';</script>";
+}else{
+    echo "<script>alert('No se pudo guardar el curso'); window.history.go(-1);</script>";
+}
 
 
 
